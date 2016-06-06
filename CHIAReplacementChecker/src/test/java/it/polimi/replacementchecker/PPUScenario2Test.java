3b33488a2a8c3af41d9e4b5c6c0638017859fa9b
claudiomenghi <cla.menghi@gmail.com>
2016-06-06 11:54:21 +0200

    Adds the tests corresponding to the case studies

---
+++ CHIAReplacementChecker/src/test/java/it/polimi/replacementchecker/PPUScenario2Test.java
@@ -0,0 +1,56 @@
+package it.polimi.replacementchecker;
+
+import static org.junit.Assert.assertTrue;
+import it.polimi.checker.SatisfactionValue;
+import it.polimi.checker.intersection.acceptingpolicies.AcceptingPolicy;
+import it.polimi.checker.intersection.acceptingpolicies.AcceptingPolicy.AcceptingType;
+import it.polimi.constraints.Constraint;
+import it.polimi.constraints.components.Replacement;
+import it.polimi.constraints.io.in.constraint.ConstraintReader;
+import it.polimi.constraints.io.in.replacement.ReplacementReader;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class PPUScenario2Test {
+
+	@Test
+	public void testScenario1Req2() throws Exception {
+		ReplacementReader modelReader = new ReplacementReader(new File(getClass().getClassLoader()
+				.getResource("ppu/scenario2/rep/ReplacementCrane.xml").getFile()));
+		Replacement model = modelReader.perform();
+
+		ConstraintReader claimReader = new ConstraintReader(new File(getClass().getClassLoader()
+				.getResource("ppu/scenario2/rep/ConstraintReq2.xml").getFile()));
+		Constraint claim = claimReader.perform();
+
+		ReplacementChecker checker = new ReplacementChecker(model, 
+				claim.getSubProperty(model.getModelState()),
+				AcceptingPolicy.getAcceptingPolicy(AcceptingType.BA, model.getAutomaton(),
+						claim.getSubProperty(model.getModelState()).getAutomaton()));
+		assertTrue(SatisfactionValue.SATISFIED==checker.perform());
+		assertTrue(75==checker.getIntersectionAutomataSize());
+		
+	}
+
+	@Test
+	public void testScenario1Req4() throws Exception {
+		ReplacementReader modelReader = new ReplacementReader(new File(getClass().getClassLoader()
+				.getResource("ppu/scenario2/rep/ReplacementCrane.xml").getFile()));
+		Replacement model = modelReader.perform();
+
+		ConstraintReader claimReader = new ConstraintReader(new File(getClass().getClassLoader()
+				.getResource("ppu/scenario2/rep/ConstraintReq4.xml").getFile()));
+		Constraint claim = claimReader.perform();
+
+		ReplacementChecker checker = new ReplacementChecker(model, 
+				claim.getSubProperty(model.getModelState()),
+				AcceptingPolicy.getAcceptingPolicy(AcceptingType.BA, model.getAutomaton(),
+						claim.getSubProperty(model.getModelState()).getAutomaton()));
+		assertTrue(SatisfactionValue.SATISFIED==checker.perform());
+		
+		assertTrue(75==checker.getIntersectionAutomataSize());
+
+	}
+}
